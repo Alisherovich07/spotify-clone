8ebc296ca4ebbcadb14e415ab2979a0f7d32cc24
M.Bek <darklord7133@gmail.com>
2024-06-30 00:58:26 +0500

    add player and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './App.css'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
 
 const App = () => {
   return (
-    <div>
-
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar />
+      </div>
+        <Player />
     </div>
   )
 }
